02629a4bfb794cb964b010808e408430d932292e
Alejandro Franco <afranco7@gmail.com>
2021-07-10 12:47:10 -0500

    getoffers

---
+++ src/test/java/Ofertas/TrabajaNosotros.java
@@ -0,0 +1,38 @@
+package Ofertas;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public class TrabajaNosotros {
+
+    private final WebDriver driver;
+
+    public TrabajaNosotros(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    By TodasLasOfertas() { return By.cssSelector("div.empty-thumb"); }
+
+    public boolean isLoaded() {
+        WebDriverWait wait = new WebDriverWait(driver,10);
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h2"))).isDisplayed();
+    }
+    
+    public void obtenerTodasLasOfertas(){
+        List<WebElement> ofertas = driver.findElements(TodasLasOfertas());
+        String nombre ="";
+        String fecha ="";
+        String descripcion ="";
+
+        for (WebElement oferta: ofertas) {
+            nombre = oferta.findElement(By.cssSelector("div.premium-blog-entry-container>h3>a")).getText();
+            fecha = oferta.findElement(By.cssSelector("div.premium-blog-entry-container>div>span>span")).getText();
+            descripcion = oferta.findElement(By.cssSelector(".premium-blog-post-content")).getText();
+        }
+    }
+}
